f07ba51b562c8c4a6598e224657eb4c75c2586ae
rarepepi <pepimartinez@protonmail.com>
2022-12-05 14:05:38 -0500

    update prompt

--- src/openai/summary.ts
+++ src/openai/summary.ts
@@ -10,8 +10,8 @@ export async function getSummary(text: string) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `Can you summarize this, keep in mind each line is one persons message: ${text}`,
-      max_tokens: 512,
+      prompt: `Can you summarize this, keep in mind each section is one person talking, as much as you can keep track of the conversation and sum it up: ${text}`,
+      max_tokens: 1024,
     });
     console.log(completion.data.choices[0].text);
     return completion.data.choices[0].text;
