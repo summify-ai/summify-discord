73e6fd4196510254b8d04b236499fb2ffad485d1
rarepepi <pepimartinez@protonmail.com>
2022-12-05 10:29:06 -0500

    simple

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,61 +1,23 @@
 // Require the necessary discord.js classes
 const { Client, Events, Collection, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
-const fs = require("node:fs");
-const path = require("node:path");
 
-const token = process.env.DISCORD_TOKEN;
-console.log("Bot is starting...");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+client.login(process.env.DISCORD_TOKEN);
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection();
-
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file: string) => file.endsWith(".js"));
-
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  // Set a new item in the Collection with the key as the command name and the value as the exported module
-  if ("data" in command && "execute" in command) {
-    client.commands.set(command.data.name, command);
-  } else {
-    console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-    );
-  }
-}
+client.on("ready", (message: any) => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on("messageCreate", async (message: any) => {
-  if (!message.content.startsWith("!") || message.author.bot) return;
-  if (message.content === "!ping") {
-    message.channel.send("Pong.");
+  if (message.content.startsWith("!ping")) {
+    message.reply("Pong!");
   }
+  console.log(message.content);
 });
-
-client.on(Events.InteractionCreate, async (interaction: any) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
-
-// Log in to Discord with your client's token
-client.login(token);
