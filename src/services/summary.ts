6fb1461a322a09935e95aacb48700d7a32cda86a
rarepepi <pepimartinez@protonmail.com>
2022-12-06 14:50:58 -0500

    new proompt3

--- src/services/summary.ts
+++ src/services/summary.ts
@@ -10,7 +10,7 @@ export async function getSummary(text: string) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: ` This is a converstation between a group on Discord: ${text}\n\n ... Can you summarize it each individual line is a message formated as "username: message". I would like you to summarize the conversation in a way that really captures the essence of the conversation and then a bullet list of the most important topics and links shared, in a non-repetitive manner in four sentences and then add a section under it with bulleted list of links extracted. \n\n--\n\n`,
+      prompt: `This is a converstation between a group on Discord: ${text}\n\n ... Can you summarize it each individual line is a message formated as "username: message". I would like you to summarize the conversation in a way that really captures the essence of the conversation in a non-repetitive manner in four sentences and then a bullet list of the most important topics and links shared. \n\n--\n\n`,
       max_tokens: 1024,
       temperature: 0.0,
     });
