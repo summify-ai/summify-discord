551d9d7ceb02b32ceded5dfa82597424d8e731b9
n0ahdoteth <n0ahdoteth@gmail.com>
2022-12-06 23:22:57 -0500

    changed prompt

--- src/services/summary.ts
+++ src/services/summary.ts
@@ -12,6 +12,7 @@ export async function getSummary(text: string) {
       model: "text-davinci-003",
       prompt: `You are a summarization bot that is designed to accurately extract peak insights from discord messages. The following text is a conversation between a group of members in a Discord server. Each individual line is a message formatted as "username: message". I would like you to summarize the conversation in a way that really captures the essence of the conversation in a non-repetitive manner in four sentences, not five sentences, not three sentences. \n\n After the summary,  create a bulleted list of the most important topics. Below that, compile and create a list of all of the links shared. ${text}`,
       max_tokens: 1024,
+      temperature: 0.1,
     });
     console.log(completion.data.choices[0].text);
     return completion.data.choices[0].text;
